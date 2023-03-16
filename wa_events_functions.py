1125eca2132f26dad232d33ee4be970030d58ee4
Creidhne86 <mbehling@rosefiji.com>
2023-03-15 21:47:57 -0500

    Update wa_events_functions.py

--- wa_events_functions.py
+++ wa_events_functions.py
@@ -226,3 +226,47 @@ def add_additional_events(ics_current_path, ics_latest_path):
             print(f'Added event_id {event_id}')
 
     return ics_current.to_ical()
+
+def upload_file_to_wildapricot(api_key, file_name, ical_data):
+    api_base_url = 'https://api.wildapricot.org/v2.1'
+    auth_url = 'https://oauth.wildapricot.org/auth/token'
+
+    # Encode the API key in base64 format
+    encoded_key = base64.b64encode(f'APIKEY:{api_key}'.encode()).decode()
+
+    # Set the headers for authentication
+    auth_headers = {
+        'Authorization': f'Basic {encoded_key}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    # Obtain the access token
+    auth_data = {'grant_type': 'client_credentials', 'scope': 'auto'}
+    auth_response = requests.post(auth_url, headers=auth_headers, data=auth_data)
+    access_token = auth_response.json()['access_token']
+
+    # Set the headers with the access token for API requests
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    # Make an API request to retrieve the account details
+    account_response = requests.get(f'{api_base_url}/accounts', headers=headers)
+    account_id = account_response.json()[0]['Id']
+
+    # Upload the iCal data
+    upload_headers = headers.copy()
+    upload_headers['Content-Type'] = 'application/octet-stream'
+
+    upload_response = requests.post(
+        f'{api_base_url}/accounts/{account_id}/filestorage/files',
+        headers=upload_headers,
+        data=ical_data,
+    )
+
+    if upload_response.status_code == 201:
+        print(f'Successfully uploaded {file_path} to WildApricot.')
+    else:
+        print(f'Failed to upload file: {upload_response.status_code}')

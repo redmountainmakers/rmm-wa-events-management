fb78e64cf12049276c01a6258913f0fdc17fd3a1
Creidhne86 <mbehling@rosefiji.com>
2023-03-15 21:39:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import os
 from wa_events_functions import*
 
 api_key = os.environ.get("API_KEY")
+access_token = get_access_token(api_key)
 
-download_and_commit()
+#download_and_commit()
+ical_data = get_upcoming_events(access_token)
+upload_file_to_wildapricot(api_key,file_name)

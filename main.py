65c7e265aaf2a19b54324cb7a923903db1da8226
Matt <mcbehling86@gmail.com>
2023-03-17 08:46:39 -0500

    updated to overwrite the redmountainmakers_events.ics instead of a new file rmm_events

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ new_ics = add_additional_events(ics_current_path, wa_ics_path)
 with open(new_ics_path, 'wb') as f:#writes the ics data locally
     f.write(new_ics)
 new_ics = update_fields(ics_current_path, wa_ics_path)
-save_ics_file(new_ics)#saves the final file as rmm events.ics
-commit_and_push("rmm_events.ics")
+save_ics_file(new_ics,"redmountainmakers_events.ics")#saves the final file as rmm events.ics
+commit_and_push("redmountainmakers_events.ics")

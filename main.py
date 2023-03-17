422bdf541a8d8d2a38b8b0fa018524837efaef27
Matt <mcbehling86@gmail.com>
2023-03-17 08:32:01 -0500

    added some saveing of the ics data so the functions should work correctly

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ current_events = 'https://github.com/Creidhne86/wa_events_2_ics/blob/main/redmou
 
 download_and_commit(current_events)#Downloads the list of current events and archives them in a repo archive
 upcoming_events = get_upcoming_events(access_token)
-wa_ics_file = create_ics_file(upcoming_events)
 ics_current_path = 'redmountainmakers_events.ics'
-new_ics = add_additional_events(ics_current_path, wa_ics_file)
-new_ics = update_fields(ics_current_path, new_ics)
+wa_ics_path = 'wa_events.ics'
+wa_ics_file = create_ics_file(upcoming_events)
+with open(wa_ics_path, 'wb') as f:
+    f.write(wa_ics_file)
+new_ics = add_additional_events(ics_current_path, wa_ics_path)
+with open(wa_ics_path, 'wb') as f:
+    f.write(new_ics)
+new_ics = update_fields(ics_current_path, wa_ics_path)
 save_ics_file(new_ics)
 commit_and_push("rmm_events.ics")

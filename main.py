6a0a98686ce13ef79eb33977cc3c95f75461c0c7
Matt Behling <mcbehling86@gmail.com>
2023-03-23 16:48:19 -0500

    Added comments, moved the API calls around to ensure they don't get exposed

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import subprocess
 from wa_events_functions import*
 
-api_key = os.environ.get("API_KEY")
-access_token = get_access_token(api_key)
-
 current_ics_url = 'https://redmountainmakers.org/resources/Events_Conversion/redmountainmakers_events.ics'
 
-upcoming_events = get_upcoming_events(access_token) #Gets the list of upcoming events from the WA API
+#Specifies the local file paths for files to be saved locally on the Actions runner
 ics_current_path = 'redmountainmakers_events.ics'
 wa_ics_path = 'wa_events.ics'
 output_ics_path = 'rmm_events.ics'
@@ -15,11 +12,17 @@ save_path_with_date = ics_current_path[:-4] + f"_{today}.ics"
 archive_ics_path = "archive/" + save_path_with_date
 log_file_path = f"archive/event_update_{today}.log"
 
-download_ics_file(current_ics_url,ics_current_path)
+download_ics_file(current_ics_url,ics_current_path)#Downloads the current ics file from the RMM website
+
+api_key = os.environ.get("API_KEY")#Gets the API key from the environment variables
+access_token = get_access_token(api_key)#Gets the access token from the WA API
+
+upcoming_events = get_upcoming_events(access_token) #Gets the list of upcoming events and data from the WA API
 create_ics_file(upcoming_events,wa_ics_path)#creates the ics file from the WA API data
-process_calendar(ics_current_path, wa_ics_path, output_ics_path,log_file_path)
 
+process_calendar(ics_current_path, wa_ics_path, output_ics_path,log_file_path)#Deletes old events, adds new events, and updates changed events
 
+#pushes the log file, archive ics file, and the updated ics file to the git repo
 subprocess.run(['git', 'add', output_ics_path])
 subprocess.run(['git', 'add', archive_ics_path])
 subprocess.run(['git', 'add', log_file_path])
